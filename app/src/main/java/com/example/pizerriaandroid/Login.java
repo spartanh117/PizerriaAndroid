5d49dc493c458248faa65b25ffa9617b5a5418f3
Manuel Lopez <manulopez117@gmail.com>
2021-01-20 20:10:49 +0100

    actilizar

--- app/src/main/java/com/example/pizerriaandroid/Login.java
+++ app/src/main/java/com/example/pizerriaandroid/Login.java
@@ -1,14 +1,34 @@
 package com.example.pizerriaandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
+    private Button iniciarSesion;
+    private EditText email,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        iniciarSesion = (Button)findViewById(R.id.btnIniciarSesion);
+        email = (EditText)findViewById(R.id.editTextTextEmailAddress);
+        password = (EditText)findViewById(R.id.editTextTextPassword);
+
+        iniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if((email.getText() != null) && (password.getText() != null)){
+                    Intent intent = new Intent (v.getContext(), PantallaPrincipal.class);
+                    startActivityForResult(intent, 0);
+                }
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/pizerriaandroid/PantallaPrincipal.java
@@ -0,0 +1,15 @@
+package com.example.pizerriaandroid;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PantallaPrincipal extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_pantalla_principal);
+        }
+}
+
